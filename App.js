b4e63b0e618b6c99b87f6e83537453850803c3a9
David Li <david.li3100@gmail.com>
2019-06-23 20:26:04 -0400

    initial navbar setup

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createBottomTabNavigator, NavigationContainer, NavigationContainerProps, NavigationRoute } from 'react-navigation';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  BottomNavigationProps,
+} from 'react-native-ui-kitten';
+// import { Ionicons } from "@expo/vector-icons";
+import {Image} from 'react-native'
 
-export default function App() {
+import VotingScreen  from './screens/voting';
+import FeedScreen  from './screens/feed';
+
+type CommonNavigationProps = NavigationProps & NavigationContainerProps;
+
+export const TabNavigatorScreen = createBottomTabNavigator(
+  {
+  Home: FeedScreen,
+  Voting: VotingScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarComponent: BottomNavigationShowcase,
+  }
+);
+
+export const BottomNavigationShowcase = (BottomNavigationProps) => {
+  const onTabSelect = (selectedIndex) => {
+    const { selectedRoute } = props.navigation.state.routes;
+
+    navigation.navigate(selectedRoute.routename);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Fraser Connect</Text>
-    </View>
-  );
+    <BottomNavigation
+      appearance='noIndicator'
+      selectedIndex={props.navigation.state.index}
+      onSelect={onTabSelect}>
+        <BottomNavigationTab icon={<Image source={require('./assets/evaicons/outline/svg/home-outline.svg')}/>} title='Home'/>
+        <BottomNavigationTab title='Voting'/>
+      </BottomNavigation>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(TabNavigatorScreen);
+
