6aff2ec8568155668365ff026c30b0b46bd2b3de
David Li <david.li3100@gmail.com>
2019-08-10 13:29:27 -0400

    create events screen

--- screens/events.js
+++ screens/events.js
@@ -1,7 +1,6 @@
 //import liraries
 import React, { Component } from "react";
-import { View, StyleSheet, Platform, StatusBar } from "react-native";
-import { default as Text } from "../components/Text";
+import { View, StyleSheet, Platform, StatusBar, Text } from "react-native";
 import Header from "../components/global/Header";
 import {
   widthPercentageToDP,
@@ -13,8 +12,12 @@ class Events extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header screenName="Events" />
-        <Text h1>Events</Text>
+        <Header headerDescription="Here's what's happening at Fraser"/>
+        <View style={styles.eventsContainer}>
+          <Text style={styles.headingText}>
+            Upcoming Events
+          </Text>
+        </View>
       </View>
     );
   }
@@ -23,9 +26,20 @@ class Events extends Component {
 // define your styles
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight
-    //   marginLeft: widthPercentageToDP('4%'),
-    //   marginRight: widthPercentageToDP('4%'),
+    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight,
+    backgroundColor: "#ffffff"
+  },
+  eventsContainer: {
+    backgroundColor: "#FAFAFC",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  headingText: {
+    marginLeft: widthPercentageToDP('4.5%'),
+    fontFamily: "Poppins-SemiBold",
+    fontSize: heightPercentageToDP('2.5%'),
+    color: '#102A43',
+    marginBottom: heightPercentageToDP('1%')
   }
 });
 
